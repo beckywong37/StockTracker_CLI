957c1641db25e5f72d4eae0f638f700e57451100
beckywong37 <becky.wong.0413@mail.linnbenton.edu>
2024-09-04 19:09:13 -0700

    test commit

---
+++ test_commit.py
@@ -0,0 +1,19 @@
+# Setting up Github repository for project
+
+"""
+Github username: beckywong37
+Step 1: Add remote repo with token
+    git remote add origin <URL>
+    git remote set-url origin <URL>
+Step 2: Check repo added correctly (will show URL with token)
+    git remote -v
+Step 3: Check name of branch
+    git branch
+Step 4: Stage changes
+    git add .
+    git status (shows which files will be committed)
+Step 5: Commit changes
+    git commit -m "Your commit message"
+Step 6: To share changes with remote repo, need to push
+    git push origin master
+"""
